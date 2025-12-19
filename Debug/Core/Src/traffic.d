4aeffb9c529b1ba2dd47e0ea8939fa6eacceb894
Malte Berg <malte@malte.nu>
2025-12-19 16:23:44 +0100

    Update Debug/

--- Debug/Core/Src/traffic.d
+++
@@ -1,5 +0,0 @@
-Core/Src/traffic.o: ../Core/Src/traffic.c ../Core/Inc/Traffic.h \
- ../Core/Inc/traffic_functions.h ../Core/Inc/Traffic.h
-../Core/Inc/Traffic.h:
-../Core/Inc/traffic_functions.h:
-../Core/Inc/Traffic.h:
